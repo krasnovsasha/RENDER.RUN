744da2baa6c7aa6abaab88e0d222c79b16f7760c
alexander <pancasancha4@gmail.com>
2020-07-17 01:59:34 +0300

    -started language localization to aboutMe page

--- languages/en.php
+++ languages/en.php
@@ -6,6 +6,19 @@
         "projects" => "projects",
         "contacts" => "contacts",
         "langRu" => "Русский",
-        "langEn" => "English"
+        "langEn" => "English",
+        "aboutMeText" => "Hello! My name is Alena,  I have been doing 3D visualization for over a year.
+            During this time, I took an artistic training course at the ArtFuture school in St. Petersburg,
+            modeling course at the Autodesk Certified Center for 3ds Max at ITMO University,
+            author's course on 3D visualization with one of the most talented 3D visualizers, Dmitry Kurilov.<br>
+            Also during this time I have completed several projects. The projects were invented by me inside and out,
+            except for the kitchen-living room project, I made it according to the reference from the Internet.<br>
+            The work on the project consisted of creating a room, setting up lighting, selecting furniture and
+            decor from the catalog, including independent modeling of individual elements, setting
+            materials, setting angles, visualization and post-processing in Photoshop. 3d visualization
+            this is what completely fascinates me, I make every effort to achieve high-quality
+            result, along the way learning new things. For me, the process of creating 3D visualization is extremely exciting.
+            a process from which it is simply impossible to break away.<br>
+            <p>To create the renders were used: 3Ds Max, Corona Renderer, Photoshop.</p>"
     );
 ?>
