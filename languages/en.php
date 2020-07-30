17dc62a3f2f9b13e8c38b82f57eda803bb6b8806
alexander <pancasancha4@gmail.com>
2020-07-31 00:22:23 +0300

    - formatted text on about me page

--- languages/en.php
+++ languages/en.php
@@ -7,18 +7,18 @@
         "contacts" => "contacts",
         "langRu" => "Русский",
         "langEn" => "English",
-        "aboutMeText" => "Hello! My name is Alena,  I have been doing 3D visualization for over a year.
-            During this time, I took an artistic training course at the ArtFuture school in St. Petersburg,
-            modeling course at the Autodesk Certified Center for 3ds Max at ITMO University,
-            author's course on 3D visualization with one of the most talented 3D visualizers, Dmitry Kurilov.<br>
+        "aboutMeText" => "Hello! My name is Alena,  I have been doing 3D visualization for over a year.<br>
+            During this time, I took an artistic training course at the <a href='https://artfuture-school.ru/' target='_blank'><i>ArtFuture</i></a> school in St. Petersburg,
+            modeling course at the Autodesk Certified Center for 3ds Max at <a href='http://cad.itmo.ru/center.php' target='_blank'><i>ITMO University</i></a> ,
+            author's course on 3D visualization with one of the most talented 3D visualizers,  Dmitry Kurilov  .<br>
             Also during this time I have completed several projects. The projects were invented by me inside and out,
             except for the kitchen-living room project, I made it according to the reference from the Internet.<br>
             The work on the project consisted of creating a room, setting up lighting, selecting furniture and
             decor from the catalog, including independent modeling of individual elements, setting
-            materials, setting angles, visualization and post-processing in Photoshop. 3d visualization
+            materials, setting angles, visualization and post-processing in Photoshop.<br> 3d visualization
             this is what completely fascinates me, I make every effort to achieve high-quality
             result, along the way learning new things. For me, the process of creating 3D visualization is extremely exciting.
-            a process from which it is simply impossible to break away.<br>
+            a process from which it is simply impossible to break away.<br><br>
             <p>To create the renders were used: 3Ds Max, Corona Renderer, Photoshop.</p>",
         "projectInstruments" => "<h5>The following tools were used in the development of the project:</h5>
                                 <p>3d modeling - 3ds Max,
